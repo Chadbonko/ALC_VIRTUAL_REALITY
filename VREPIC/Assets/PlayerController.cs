7ab61fef77d5194e35e45d07be77fab64a1d3990
Bruce Lee <bruce.l640@stu.nebo.edu>
2020-01-21 10:29:20 -0700

    jkg

---
+++ VREPIC/Assets/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+    public float forwardInput;
+    public float horizontalInput;
+    public float turnSpeed = 10f;
+    private float gravityModifier = 1.5f;
+    public float jumpForce;
+    public bool isOnGround;
+    public Rigidbody playerRb;
+    private Animator playerAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnim = GetComponent<Animator>();
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This code makes the player walk and jump using arrow keys and space.
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+
+    }
+}
